430988d48ea4af46d2694f51bc8f9fa6cae9f631
Utso Sarkar <utsosarkar7870@gmail.com>
2026-09-09 10:59:02 +0600

    dev: home and network sidebar update

---
+++ app/(Frond-End)/mu/(jobs)/jobs/subscription/page.tsx
@@ -0,0 +1,12 @@
+import PlanPricingTable from '@/app/(Frond-End)/_components/adminDashboard/subscription/planPricingCard/PlanPricingTable'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+          <PlanPricingTable/>
+    </div>
+  )
+}
+
+export default page
